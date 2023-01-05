cd3dc05384954e28ecb1d9e9105291c727b7a796
Timothy <gdslink.timothycruz@gmail.com>
2023-01-05 19:01:47 +0800

    first commit

---
+++ QandA_Program.py
@@ -0,0 +1,238 @@
+
+message = '''Ahoy There, Me Hearties! ٩(•̮̮̃-̃)۶🏴‍☠️
+We're in 🔍 search for a new captain who is good in multiplying 'cause we're in quest to answer the 
+multiplication questions that magical dragon 🐉 asks, so it can multiply our treasures 💰 like how we wanted.  
+
+We suggest you choose to train first 💪 before you can proceed to ace the dragon's test! ✊
+You only have 3 chances for each question, Goodluck!👌\n
+Here's one question for you, mate. Prove that your worth, eh?🐋\n'''  # first game question
+print(message)
+
+
+def loopQuestion():  # loop function
+
+    answer = 14
+    guess_count = 0
+    guess_limit = 3
+    while guess_count < guess_limit:  # loopings of the games
+        guess = int(input('2 x 7 = '))
+        guess_count += 1
+        if guess == answer:  # print if the the user is correct
+            print('\n"Yo ho ho! Shiver me timbers! That was right, matey. Do it again!😎"')
+            break
+    else:  # print if the the user is not correct
+        print('\n"Oh my, oh me. You got that wrong, matey. Try again, eh?✌✌"\n')
+        loopQuestion()
+
+
+loopQuestion()  # end of loop
+
+# second game question
+print("\nAnother one! You might as well get it right, mate.🐠\n")
+
+
+def loopQuestion():  # loop function
+
+    answer = 9
+    guess_count = 0
+    guess_limit = 3
+    while guess_count < guess_limit:  # loopings of the games
+        guess = int(input('3 x 3 = '))
+        guess_count += 1
+        if guess == answer:  # print if the the user is correct
+            print('''\n"Avast Ye, hearties! This lad got that correctly. Let's go!🤯"\n''')
+            break
+    else:  # print if the the user is not correct
+        print('\n"Bring the luck back, you gotta do your best. Try again, heartie!✌✌"\n')
+        loopQuestion()
+
+
+loopQuestion()  # end of loop
+
+# third game question
+print("\nLet's try an easier one. Don't you disappoint us, hearties!🐬\n")
+
+
+def loopQuestion():  # loop function
+
+    answer = 24
+    guess_count = 0
+    guess_limit = 3
+    while guess_count < guess_limit:  # loopings of the games
+        guess = int(input('4 x 6 = '))
+        guess_count += 1
+        if guess == answer:       # print if the the user is correct
+            print('''\n"Wo-wo-wo! Loot that correct information, we gonna need that! More, more, we gotta learn some more!😁"\n''')
+            break
+    else:           # print if the the user is not correct
+        print('''\n"Goodness me! You hadn't got it right, mate. Try one more time.✌✌"\n''')
+        loopQuestion()
+
+
+loopQuestion()  # end of loop
+
+# fourth game question
+print("\nGood, good! How about something more difficult, eh?🦀\n")
+
+
+def loopQuestion():  # loop function
+
+    answer = 40
+    guess_count = 0
+    guess_limit = 3
+    while guess_count < guess_limit:  # loopings of the games
+        guess = int(input('5 x 8 = '))
+        guess_count += 1
+        if guess == answer:       # print if the the user is correct
+            print('''\n"Avast Ye, hearties! This lad got that correctly. Let's go!🧐"\n''')
+            break
+    else:           # print if the the user is not correct
+        print(
+            '''\n"Bring the luck back, you gotta do your best. Try again, heartie!✌✌"\n''')
+        loopQuestion()
+
+
+loopQuestion()  # end of loop
+
+# fifth game question
+print("\nYou're tough, mate. Can you also do this?🦐\n")
+
+
+def loopQuestion():  # loop function
+
+    answer = 54
+    guess_count = 0
+    guess_limit = 3
+    while guess_count < guess_limit:  # loopings of the games
+        guess = int(input('6 x 9 = '))
+        guess_count += 1
+        if guess == answer:       # print if the the user is correct
+            print('''\n"Wo-wo-wo! Loot that correct information, we gonna need that! More, more, we gotta learn some more!🤓"\n''')
+            break
+    else:           # print if the the user is not correct
+        print('''\n"Oh my, oh me. You got that wrong, matey. Try again, eh?✌✌"\n''')
+        loopQuestion()
+
+
+loopQuestion()  # end of loop
+
+# sixth game question
+print("\nWoah, woah! You wanted some gold, ye? Better answer it right.🐡\n")
+
+
+def loopQuestion():  # loop function
+
+    answer = 21
+    guess_count = 0
+    guess_limit = 3
+    while guess_count < guess_limit:  # loopings of the games
+        guess = int(input('7 x 3 = '))
+        guess_count += 1
+        if guess == answer:       # print if the the user is correct
+            print(
+                '''\n"Yo ho ho! Shiver me timbers! That was right, matey. Do it again!🤩"\n''')
+            break
+    else:           # print if the the user is not correct
+        print('''\n"Goodness me! You hadn't got it right, mate. Try one more time.✌✌"\n''')
+        loopQuestion()
+
+
+loopQuestion()  # end of loop
+
+# seventh game question
+print("\nA great captain must be able to answer this. Are you a great one?🐟\n")
+
+
+def loopQuestion():  # loop function
+
+    answer = 49
+    guess_count = 0
+    guess_limit = 3
+    while guess_count < guess_limit:  # loopings of the games
+        guess = int(input('7 x 7 = '))
+        guess_count += 1
+        if guess == answer:       # print if the the user is correct
+            print(
+                '''\n"Avast Ye, hearties! This lad got that correctly. Let's go!🤗"\n''')
+            break
+    else:           # print if the the user is not correct
+        print('''\n"Oh my, oh me. You got that wrong, matey. Try again, eh?✌✌"\n''')
+        loopQuestion()
+
+
+loopQuestion()  # end of loop
+
+# eighth game question
+print("\nYou must possess great wisdom in order to lead. Are you qualified? Can you do this, lad?🐳\n")
+
+
+def loopQuestion():  # loop function
+
+    answer = 32
+    guess_count = 0
+    guess_limit = 3
+    while guess_count < guess_limit:  # loopings of the games
+        guess = int(input('8 x 4 = '))
+        guess_count += 1
+        if guess == answer:       # print if the the user is correct
+            print(
+                '''\n"Yo ho ho! Shiver me timbers! That was right, matey. Do it again!🤠"\n''')
+            break
+    else:           # print if the the user is not correct
+        print(
+            '''\n"Bring the luck back, you gotta do your best. Try again, heartie!✌✌"\n''')
+        loopQuestion()
+
+
+loopQuestion()  # end of loop
+
+# ninth game question
+print("\nQuite the knowledge you possess, I suppose you can answer this correctly?🦑\n")
+
+
+def loopQuestion():  # loop function
+
+    answer = 63
+    guess_count = 0
+    guess_limit = 3
+    while guess_count < guess_limit:  # loopings of the games
+        guess = int(input('9 x 7 = '))
+        guess_count += 1
+        if guess == answer:       # print if the the user is correct
+            print(
+                '''\n"Wo-wo-wo! Loot that correct information, we gonna need that! More, more, we gotta learn some more!😍"\n''')
+            break
+    else:           # print if the the user is not correct
+        print(
+            '''\n"Goodness me! You hadn't got it right, mate. Try one more time.✌✌"\n''')
+        loopQuestion()
+
+
+loopQuestion()  # end of loop
+
+# tenth game question
+print("\nIf you can get this right, mate, you'll be our new captain. Give it your all!🐠\n")
+
+
+def loopQuestion():  # loop function
+
+    answer = 100
+    guess_count = 0
+    guess_limit = 3
+    while guess_count < guess_limit:  # loopings of the games
+        guess = int(input('10 x 10 = '))
+        guess_count += 1
+        if guess == answer:       # print if the the user is correct
+            print(
+                '''\n"Avast Ye, hearties! This lad got that correctly. Let's go!🤩👏"\n''')
+            break
+    else:           # print if the the user is not correct
+        print(
+            '''\n"Oh my, oh me. You got that wrong, matey. Try again, eh?✌✌"\n''')
+        loopQuestion()
+
+
+loopQuestion()  # end of loop
+
+print('''Yo ho ho! Amazing talent you got here, matey!🏴‍☠️We welcome you aboard as our new captain.٩(•̮̮̃-̃)۶👋 
+You're ready to set sail in pursuit of that magical dragon!🐉🦖''')
